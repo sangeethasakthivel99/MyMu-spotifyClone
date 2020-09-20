72ae1ada3d0d949e2647e33da4889e0756f6b09d
Sangeetha <sangeetha@testpress.in>
2020-09-20 13:55:04 +0530

    Add Dagger Setup

---
+++ app/src/main/java/com/sangeetha/mymu_spotifyclone/di/ServiceModule.kt
@@ -0,0 +1,43 @@
+package com.sangeetha.mymu_spotifyclone.di
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideAudioAttributes(): AudioAttributes {
+        return AudioAttributes.Builder()
+            .setContentType(C.CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideExoplayer(@ApplicationContext context: Context, audioAttributes: AudioAttributes): SimpleExoPlayer {
+        return SimpleExoPlayer.Builder(context).build().apply {
+            setAudioAttributes(audioAttributes, true)
+                setHandleAudioBecomingNoisy(true)
+        }
+    }
+
+    @ServiceScoped
+    @Provides
+    fun provideDataSourceFactory(@ApplicationContext context: Context): DefaultDataSourceFactory {
+        return DefaultDataSourceFactory(context, Util.getUserAgent(context, "MyMu"))
+    }
+}
