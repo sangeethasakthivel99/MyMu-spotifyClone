6cb5684ed1ecf591e902179ed25fc8e6352b27cb
Sangeetha <sangeetha@testpress.in>
2020-10-01 18:47:45 +0530

    Add Callbacks

--- app/src/main/java/com/sangeetha/mymu_spotifyclone/exoplayer/callbacks/MusicPlaybackPreparer.kt
+++ app/src/main/java/com/sangeetha/mymu_spotifyclone/exoplayer/callbacks/MusicPlaybackPreparer.kt
@@ -4,35 +4,40 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.ResultReceiver
 import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import com.google.android.exoplayer2.ControlDispatcher
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
-import com.sangeetha.mymu_spotifyclone.exoplayer.FirebaseMusicService
+import com.sangeetha.mymu_spotifyclone.exoplayer.FirebaseMusicSource
 
 class MusicPlaybackPreparer(
-    private val firebaseMusicSource: FirebaseMusicService,
+    private val firebaseMusicSource: FirebaseMusicSource,
     private val playerPrepared: (MediaMetadataCompat?) -> Unit
-): MediaSessionConnector.PlaybackPreparer  {
+) : MediaSessionConnector.PlaybackPreparer {
+
     override fun onCommand(
         player: Player,
         controlDispatcher: ControlDispatcher,
         command: String,
         extras: Bundle?,
         cb: ResultReceiver?
-    ): Boolean = false
+    ) = false
 
     override fun getSupportedPrepareActions(): Long {
-        TODO("Not yet implemented")
+        return PlaybackStateCompat.ACTION_PREPARE_FROM_MEDIA_ID or
+                PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID
     }
 
     override fun onPrepare(playWhenReady: Boolean) = Unit
 
     override fun onPrepareFromMediaId(mediaId: String, playWhenReady: Boolean, extras: Bundle?) {
-        TODO("Not yet implemented")
+        firebaseMusicSource.whenReady {
+            val itemToPlay = firebaseMusicSource.songs.find { mediaId == it.description.mediaId }
+            playerPrepared(itemToPlay)
+        }
     }
 
     override fun onPrepareFromSearch(query: String, playWhenReady: Boolean, extras: Bundle?) = Unit
 
     override fun onPrepareFromUri(uri: Uri, playWhenReady: Boolean, extras: Bundle?) = Unit
-
 }
